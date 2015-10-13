438bd5dd432ed5f34c6c128ddafc014989b35576
AJ Robertson <aj@clearsightstudio.com>
2015-10-12 17:51:19 -0700

    Fixes typo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -7,7 +7,7 @@ defmodule Rumbl.Repo do
   def all(Rumbl.User) do
     [%Rumbl.User{id: "1", name: "Jose", username: "josevalim", password: "Elixir"},
      %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
-     %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"},]
+     %Rumbl.User{id: "3", name: "Chris", username: "chrismccord", password: "phx"}]
   end
 
   def all(_module), do: []
@@ -16,7 +16,7 @@ defmodule Rumbl.Repo do
     Enum.find all(module), fn map -> map.id == id end
   end
 
-  def get_by(module, param) do
+  def get_by(module, params) do
     Enum.find all(module), fn map ->
       Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
     end
